601c4d8e3b6df79ab112e9fd7473f1efbb2cfc03
Will Wang <willy.j.wang@gmail,com>
2022-08-04 14:04:22 -0500

    Complete define_and_id

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,18 @@
 
 # YOU DO the rest:
 
-
+#The include? method determines if the string argument is included in the string the method is called on, will return true
+#because Hello World includes "Hello"
 "Hello World".include?("Hello")
 
-
+#Checks to see if the string the method is called on ends with the given string argument, will return false
 "Hello World".end_with?("Hello")
 
-
+# Same as above except returns true because "Hello World" ends with "rld"
 "Hello World".end_with?("rld")
 
-
+#Checks to see if the integer 12 is even, will return true because 12 is even
 12.even?
 
-
+#next returns the next consecutive integer, will return 19
 18.next
-
-
